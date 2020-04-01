15f180d618cba1e7930ea07efcc1fbeea3df8bd4
Vangeepuram <vangeepuram@Raghus-MacBook-Air.local>
2020-04-01 01:01:22 -0700

    calculator and its test class

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""
+alculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
